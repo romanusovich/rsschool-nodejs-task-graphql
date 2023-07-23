d83f64d544b758616f342cfc80f71a8645beba47
romanusovich1 <romanusovich1@gmail.com>
2023-07-23 10:54:01 +0300

    feat: add schema for user

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,7 @@
 import { Type } from '@fastify/type-provider-typebox';
+import * as graphql from 'graphql';
+import { UUIDType } from './types/uuid.js';
+import { createUserType, userType } from './types/user.js';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +21,46 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const queryType = new graphql.GraphQLObjectType({
+  name: "Query",
+  fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: UUIDType },
+      },
+    },
+    users: {
+      type: new graphql.GraphQLList(userType),
+    },
+  },
+});
+
+// Define the Query type
+const mutationType = new graphql.GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: userType,
+      args: {
+        user: { type: createUserType },
+      },
+    },
+    updateUser: {
+      type: userType,
+      args: {
+        id: { type: UUIDType },
+        user: { type: createUserType },
+      },
+    },
+    deleteUser: {
+      type: UUIDType,
+      args: {
+        user: { type: UUIDType },
+      },
+    },
+  },
+});
+
+export const schema = new graphql.GraphQLSchema({ query: queryType, mutation: mutationType })
